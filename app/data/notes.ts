0e986b7265db0eba9488210877795dadf92340d7
Adedamola Emmanuel <mgcrons@gmail.com>
2022-12-16 20:53:40 +0100

    final version

--- app/data/notes.ts
+++ app/data/notes.ts
@@ -1,7 +1,15 @@
 import fs from 'fs/promises'
 
 export async function getStoredNotes() {
-    const rawContent  = await fs.readFile('notes.json', {encoding:'utf-8'})
+    let rawContent
+    try {
+        rawContent = await fs.readFile('notes.json', { encoding: 'utf-8' })
+    } catch (err) {
+        await fs.appendFile('notes.json', '{ "notes": [] }')
+    } finally {
+        rawContent = await fs.readFile('notes.json', { encoding: 'utf-8' })
+}
+    
     const data = JSON.parse(rawContent);
     const storedNotes = data.notes ?? [];
     return storedNotes;
